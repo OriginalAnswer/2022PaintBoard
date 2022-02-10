668ff8d1c9d3dd2b4ff8f3be8f1127aed722fd54
OriginalAnswer <akiseonair@gmail.com>
2022-02-10 16:50:34 +0900

    up

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const cnvs = document.getElementById('jscnvs');
 const ctx = cnvs.getContext('2d');
+const colors = document.getElementsByClassName('jscolor');
+const range = document.getElementById('jsrange');
+const mode = document.getElementById('jsmode'); 
+const save = document.getElementById('jssave'); 
+const FIRST_COLOR = 'rgb(0,0,0)';
+const CNVS_SIZE_W = cnvs.offsetWidth; 
+const CNVS_SIZE_H = cnvs.offsetHeight; 
 
+cnvs.width = CNVS_SIZE_W;
+cnvs.height = CNVS_SIZE_H;
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, CNVS_SIZE_W, CNVS_SIZE_H);
 
-ctx.strokeStyle = 'rgb(0,0,0)';
+ctx.strokeStyle = FIRST_COLOR;
+ctx.fillStyle = FIRST_COLOR;
 ctx.lineWidth = 2.5;
 
-cnvs.width = cnvs.offsetWidth;
-cnvs.height = cnvs.offsetHeight;
-
 let painting = false;
+let filling = false;
 
 function startPainting(e) {painting = true;}
 function stopPainting(e) {painting = false;}
@@ -27,16 +37,63 @@ function onMouseMove(e) {
 
 function onMouseEnter(e) {
     const x = e.offsetX;
-    const y = e.offsetY;
-    
+    const y = e.offsetY;    
     ctx.moveTo(x, y);//선 시작 좌표 // Path의 시작좌표
-    
 }
 
+function rangeChange(e) {
+    const size = e.target.value;
+    ctx.lineWidth = size;
+}
+function clickColor(e) {
+    const color = e.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color
+}
+function clickMode(e) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = 'Fill';
+    } else {
+        filling = true;
+        mode.innerText = 'Paint';
+        ctx.fillStyle = ctx.strokeStyle;
+    }
+}
+function clickSave(e) {
+    const img = cnvs.toDataURL();//Default는 png
+    const link = document.createElement('a');
+    link.href = img; // url
+    link.download = "YourPainting🎨"; //다운로드 파일 이름
+    link.click();
+}
+function clickCnvs() {
+    if (filling) {
+        ctx.fillRect(0, 0, CNVS_SIZE_W, CNVS_SIZE_H);
+    }
+}
+
+function clickMouseR(event) {
+    event.preventDefault();
+}
 if (cnvs) {
-    cnvs.addEventListener("mousemove",onMouseMove);
     cnvs.addEventListener("mousedown",startPainting);
     cnvs.addEventListener("mouseup",stopPainting);
-    cnvs.addEventListener("mouseenter",onMouseEnter);
-    // cnvs.addEventListener("mouseleave",stopPainting);
+    cnvs.addEventListener("mousemove",onMouseMove);
+    cnvs.addEventListener("mouseenter", onMouseEnter);
+    cnvs.addEventListener("click", clickCnvs);
+    cnvs.addEventListener("contextmenu", clickMouseR);//마우스 우클릭 통제
+}
+
+Array.from(colors).forEach(c => c.addEventListener('click', clickColor));
+
+if (range) {
+    range.addEventListener('input', rangeChange);
+}
+
+if (mode) {
+    mode.addEventListener('click', clickMode);
+}
+if (save) {
+    save.addEventListener('click', clickSave);
 }
